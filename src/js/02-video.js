f164041acc01060f9c14edc7ffd8e802a88ac369
Jonathan Alexis Jiménez Triviño <jonnyj1216@gmail.com>
2023-07-27 19:27:42 -0500

    ejercicio 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function play(data) {
+  localStorage.setItem('reproductor', data.seconds);
+}
+
+player
+  .setCurrentTime(localStorage.getItem('reproductor'))
+  .then(function (time) {
+    console.log(time);
+    time = localStorage.getItem('reproductor');
+  });
+
+player.on('timeupdate', throttle(play, 100));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
 
+let getForm = document.querySelector('.feedback-form');
+
+const throttle = throttle(handleSubmit, 500);
+
+getForm.addEventListener('submit', handleSubmit, throttle);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+  if (email.value === '' || message.value === '') {
+    return alert('Diliencia todos los campos del formulario');
+  }
+  console.log(`Email: ${email.value} Message: ${message.value}`);
+  getForm.reset();
+}
